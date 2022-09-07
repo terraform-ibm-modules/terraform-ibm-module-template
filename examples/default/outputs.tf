571dc02ef86a7d92af862e3895fae6c22be6ff70
terraform-ibm-modules-ops <Terraform.IBM.Modules.Operations@ibm.com>
2022-09-07 11:05:40 +0000

    docs: committing files modified by the hook

--- examples/default/outputs.tf
+++ examples/default/outputs.tf
@@ -6,3 +6,13 @@ output "vpc_id" {
   description = "ID of VPC created"
   value       = ibm_is_vpc.vpc.id
 }
+
+output "resource_group_name" {
+  description = "Resource group name"
+  value       = module.resource_group.resource_group_name
+}
+
+output "resource_group_id" {
+  description = "Resource group ID"
+  value       = module.resource_group.resource_group_id
+}
