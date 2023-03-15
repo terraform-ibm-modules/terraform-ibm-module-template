004f7356c80310b16e65450fd4c6aaf6303edc49
Md-Anam-Raihan <md.anam.raihan@ibm.com>
2023-03-15 07:25:57 +0000

    fix: update variable

--- examples/non-vpc/variables.tf
+++ examples/non-vpc/variables.tf
@@ -21,9 +21,3 @@ variable "resource_group" {
   description = "An existing resource group name to use for this example, if unset a new resource group will be created"
   default     = null
 }
-
-variable "resource_tags" {
-  type        = list(string)
-  description = "Optional list of tags to be added to created resources"
-  default     = []
-}
