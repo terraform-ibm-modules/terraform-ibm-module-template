e7cbda6083f60e5a94a6edae0babd685799b6616
Md-Anam-Raihan <md.anam.raihan@ibm.com>
2023-03-15 06:12:22 +0000

    fix: updates example

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -8,13 +8,3 @@ module "resource_group" {
   resource_group_name          = var.resource_group == null ? "${var.prefix}-resource-group" : null
   existing_resource_group_name = var.resource_group
 }
-
-##############################################################################
-# VPC
-##############################################################################
-
-resource "ibm_is_vpc" "vpc" {
-  name           = "${var.prefix}-vpc"
-  resource_group = module.resource_group.resource_group_id
-  tags           = var.resource_tags
-}
