c1a9f824bd0963fcbacc9c3590857407c59f9cf5
ocofaigh <ocofaigh@ie.ibm.com>
2023-08-15 10:14:35 +0100

    mod template cleanup

--- version.tf
+++ version.tf
@@ -1,7 +1,8 @@
 terraform {
   required_version = ">= 1.3.0"
+  # If your module requires any terraform providers, uncomment the "required_providers" section below and add all required providers.
+  # Each required provider's version should be a flexible range to future proof the module's usage with upcoming minor and patch versions.
 
-  # Use a flexible range in modules that future proofs the module's usage with upcoming minor and patch versions
   #  required_providers {
   #    ibm = {
   #      source  = "IBM-Cloud/ibm"
