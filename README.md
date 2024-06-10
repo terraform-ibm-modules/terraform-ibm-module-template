e9292ec02968d9c0c23a1b4679e602ca52ab6b17
akocbek@ie.ibm.com <akocbek@ie.ibm.com>
2024-06-10 11:37:07 +0100

    fix: remove upper limit for required terraform version

--- README.md
+++ README.md
@@ -95,7 +95,7 @@ statement instead the previous block.
 
 | Name | Version |
 |------|---------|
-| <a name="requirement_terraform"></a> [terraform](#requirement\_terraform) | >= 1.3.0, <1.7.0 |
+| <a name="requirement_terraform"></a> [terraform](#requirement\_terraform) | >= 1.3.0 |
 
 ### Modules
 
--- version.tf
+++ version.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 1.3.0, <1.7.0"
+  required_version = ">= 1.3.0"
   # If your module requires any terraform providers, uncomment the "required_providers" section below and add all required providers.
   # Each required provider's version should be a flexible range to future proof the module's usage with upcoming minor and patch versions.
 
